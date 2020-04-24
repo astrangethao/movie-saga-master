12cc5275605421d0a76dfc5734117d65e2815abc
Andrew Nguyen <anguyen1320@gmail.com>
2020-04-24 16:17:23 -0500

    created the pages for movies, details, and edit and routed the components to paths

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+import MoviesPage from "../pages/MoviesPage/MoviesPage";
+import DetailsPage from "../pages/DetailsPage/DetailsPage";
+import EditPage from "../pages/EditPage/EditPage";
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <Router>
+          <header className="App-header">
+            <h1 className="App-title">Movie Database</h1>
+          </header>
+          <br />
+
+          <Route exact path="/" component={MoviesPage} />
+          <Route exact path="/details" component={DetailsPage} />
+          <Route exact path="/edit" component={EditPage} />
+        </Router>
       </div>
     );
   }
