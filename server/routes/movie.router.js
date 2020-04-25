a0a82961ad358e4bdbab1a64f7fd1e410a9e9955
Andrew Nguyen <anguyen1320@gmail.com>
2020-04-24 20:01:48 -0500

    created the a get route to get movie title, movie id, and movie genres

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -10,6 +10,22 @@ router.get("/", (req, res) => {
   pool
     .query(queryText)
     .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((error) => {
+      console.warn(`Error in SELECT movie query`, error);
+      res.sendStatus(500);
+    });
+});
+
+router.get("/genre", (req, res) => {
+  const queryText = `SELECT movies.id, movies.title as "movie_title", genres.name as "genre_name" FROM movies
+  JOIN movie_genre ON movies.id = movie_genre.movies_id
+  JOIN genres ON genres.id = movie_genre.genre_id;`;
+
+  pool
+    .query(queryText)
+    .then((response) => {
       console.log(response.rows);
       res.send(response.rows);
     })
